988af3ce5d1b0fa7cd15484fddb1a36e6a0c1598
dievenegas <diegovenegasanabalon@gmail.com>
2022-11-17 19:54:23 -0300

    Agregado Prototipo de ColisionPared() y algunos Mejoras en Clases

--- Pool/src/pool/Bola.java
+++ Pool/src/pool/Bola.java
@@ -10,7 +10,6 @@ public abstract class Bola {
     protected double velocidadX;
     protected double velocidadY;
     protected boolean estado; // Si esta en la mesa o en una tronera
-    protected final int radio;
     protected int puntaje;
     
     //METODOS
@@ -24,11 +23,6 @@ public abstract class Bola {
         return x;
     }
     
-    public void setXY(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-    
     public void setX(int x){
         this.x = x;
     }
@@ -36,6 +30,35 @@ public abstract class Bola {
         this.y = y;
     }
     
+    public void setXY(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getVelocidadX(){
+        return velocidadX;
+    }
+    
+    public double getVelocidadY(){
+        return velocidadY;
+    }
+    
+    public void setVelocidadX(double velocidadX){
+        this.velocidadX = velocidadX;
+    }
+    
+    public void setVelocidadY(double velocidadY){
+        this.velocidadY = velocidadY;
+    }
+    
+    public boolean getEstado(){
+        return estado;
+    }
+    
+    public void setEstado(boolean estado){
+        this.estado = estado;
+    }
+        
     public int getPuntaje(){
         return puntaje;
     }
@@ -46,7 +69,6 @@ public abstract class Bola {
         velocidadX = 0;
         velocidadY = 0;
         estado = true; // Si esta en la mesa o no
-        radio = 15;
         this.puntaje = puntaje;
     }
 }
--- Pool/src/pool/ConjuntoBolas.java
+++ Pool/src/pool/ConjuntoBolas.java
@@ -1,4 +1,5 @@
 package pool;
+
 //import java.util.Random;
 import static angular.Angular.distEntre2Puntos;
 import java.awt.Graphics;
@@ -11,42 +12,56 @@ public class ConjuntoBolas {
     private final ArrayList<Bola> conjunto;
     
     //METODOS
-    private boolean DetectarColision(Bola b1, Bola b2){
-        double Aux = distEntre2Puntos(b1.getX(), b1.getY(), b2.getX(), b2.getY());
-        if(Aux <= 30){
-            return true;
-        }else{
-            return false;
+    public void ColisionSimple(Bola b1){ //Colision entre Bola Pared
+        if(b1.getEstado()==true){
+            if((b1.getX()<100 && b1.getVelocidadX()<0) || (b1.getX()+30>1164 && b1.getVelocidadX()>0)){
+                b1.setVelocidadX(-b1.getVelocidadX());
+            }
+            if((b1.getY()<100 && b1.getVelocidadY()<0) || (b1.getY()+30>581 && b1.getVelocidadY()>0)){
+                b1.setVelocidadY(-b1.getVelocidadY());
+            }
         }
     }
     
-    public void ColisionSimple(){
-        
+    public void ColisionBolas(Bola b1, Bola b2){
+        if(DetectarColision(b1, b2)){
+            AnguloColision(b1, b2);
+        }
     }
     
-    public void AnguloColision(){
+    private boolean DetectarColision(Bola b1, Bola b2){ //Colision entre dos Bolas
+        if(b1.getEstado()==true && b2.getEstado()==true){
+            double Aux = distEntre2Puntos(b1.getX(), b1.getY(), b2.getX(), b2.getY());
+            if(Aux <= 30){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void AnguloColision(Bola b1, Bola b2){
     }
     
     private void RandomizarBolas(){
         for(int i=0 ; i<conjunto.size() ; i++){
             conjunto.get(i).setXY((int)(Math.random()*1034+100), (int)(Math.random()*451+100));
+            conjunto.get(i).estado = true;
         }
         for(int i=0 ; i<9; i++){
             for(int j=0 ; j<9; j++){
-                if(DetectarColision(conjunto.get(i), conjunto.get(j)) == true && i!= j){
-                    System.out.println("Detectaada colision en el primer randomizer");
-                    int aux = 0;
+                if((DetectarColision(conjunto.get(i), conjunto.get(j)) == true) && i != j){
+                    System.out.println("Detectada colision en el primer randomizer");
+                    boolean aux;
                     do{
-                        conjunto.get(i).setX((int)(Math.random()*1064+100));
-                        conjunto.get(i).setY((int)(Math.random()*481+100));
-                        aux = 0;
-                            for(int k = 0; k<9;++k){
-                                if(DetectarColision(conjunto.get(i), conjunto.get(k))&& i != k){
-                                    System.out.println("Detectaada colision en el segundo randomizer");
-                                    aux = 1; 
+                        conjunto.get(i).setXY((int)(Math.random()*1034+100), (int)(Math.random()*451+100));
+                        aux = false;
+                            for(int k=0; k<9; k++){
+                                if((DetectarColision(conjunto.get(i), conjunto.get(k)) == true) && i != k){
+                                    System.out.println("Detectada colision en el segundo randomizer");
+                                    aux = true; 
                                 }
                             }
-                    }while(aux == 1);
+                    }while(aux == true);
                 }
             }
         }
@@ -72,27 +87,6 @@ public class ConjuntoBolas {
         conjunto.add(new Bola7(0, 0));
         conjunto.add(new Bola8(0, 0));
         RandomizarBolas();
-        
-        /*
-        for(int i = 0; i<9; ++i){
-            for(int j = 0; j<9;++j){
-                if(c.get(i).DetectarColision(c.get(i), c.get(j)) &&  i != j){
-                    System.out.println("aaa");
-                    boolean aux = false;        
-                    do{
-                        c.get(i).setX((int)(Math.random()*1064+100));
-                        c.get(i).setY((int)(Math.random()*481+100));
-                        aux = false;
-                            for(int k = 0; k<9;++k){
-                                aux = c.get(i).DetectarColision(c.get(i), c.get(k));
-                                if(aux){
-                                    break;
-                                }
-                            }
-                    }while(aux == true);
-                }
-            }
-        }*/
     }
 }
 
